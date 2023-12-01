41e340b2e6a80014487686b9b67d32b42f0c3574
Agility6 <agility1013@gmail.com>
2023-12-01 13:00:09 +0800

    lab3 done

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -1,5 +1,6 @@
 package randomizedtest;
 
+import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdRandom;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -9,4 +10,64 @@ import static org.junit.Assert.*;
  */
 public class TestBuggyAList {
   // YOUR TESTS HERE
+
+    @Test
+    public void testThreeAddThreeRemove() {
+
+        AListNoResizing<Integer> aListNoResizing = new AListNoResizing<>();
+        BuggyAList<Integer> buggyAList = new BuggyAList<>();
+
+        for (int i = 4; i < 7; i++) {
+            aListNoResizing.addLast(i);
+            buggyAList.addLast(i);
+        }
+
+        for (int i = 0; i < 4; i++) {
+            assertEquals(aListNoResizing.removeLast(), buggyAList.removeLast());
+        }
+
+    }
+
+    @Test
+    public void randomizedTest() {
+
+        BuggyAList<Integer> ba = new BuggyAList<>();
+        AListNoResizing<Integer> anr = new AListNoResizing<>();
+
+        int N = 5000;
+        for (int i = 0; i < N; i += 1) {
+            int operationNumber = StdRandom.uniform(0, 4);
+            if (operationNumber == 0) {
+                // addLast
+                int randVal = StdRandom.uniform(0, 100);
+                ba.addLast(randVal);
+                anr.addLast(randVal);
+                System.out.println("addLast(" + randVal + ")");
+            } else if (operationNumber == 1) {
+                // size
+                int size = ba.size();
+                int size2 = anr.size();
+                System.out.println("size: " + size + " size2: " + size2);
+                assertEquals(size, size2);
+            } else if (operationNumber == 2) {
+                // getLast
+                if (ba.size() > 0 && anr.size() > 0) {
+                    int ret = ba.getLast();
+                    int ret2 = anr.getLast();
+                    System.out.println("getLast(" + ret + ")" + " getLast(" + ret2 + ")");
+                    assertEquals(ret, ret2);
+                }
+            } else if (operationNumber == 3) {
+                // removeLast
+                if (ba.size() > 0 && anr.size() > 0) {
+                    int ret = ba.removeLast();
+                    int ret2 = anr.removeLast();
+                    System.out.println("removeLast(" + ret + ")" + " removeLast(" + ret2 + ")");
+                    assertEquals(ret, ret2);
+                }
+            }
+        }
+
+
+    }
 }
--- lab3/timingtest/TimeAList.java
+++ lab3/timingtest/TimeAList.java
@@ -1,5 +1,7 @@
 package timingtest;
+import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.Stopwatch;
+import org.checkerframework.checker.units.qual.A;
 
 /**
  * Created by hug.
@@ -23,5 +25,35 @@ public class TimeAList {
 
     public static void timeAListConstruction() {
         // TODO: YOUR CODE HERE
+        AList<Integer> N = new AList<>();
+        // 数据的大小
+        /**
+         * 1000 2^0 * 1000 = 1000
+         * 2000 2^1 * 1000 = 2000
+         * 4000 2^2 * 1000 = 4000
+         * 8000 2^3 * 1000 = 8000
+         * ...
+         */
+        AList<Integer> Ns = new AList<>();
+        // ops计时实验期间对addLast的调用次数
+        AList<Double> times = new AList<>();
+        // 完成addLast平均花费时间
+        AList<Integer> opCounts = new AList<>();
+
+        Stopwatch stopwatch = new Stopwatch();
+        int ops = 0;
+        int tick = 0;
+        for (int i = 0; i < 1024000; i++) {
+            N.addLast(i);
+            ops += 1;
+            if (N.size() == Math.pow(2, tick) * 1000) {
+                Ns.addLast(N.size());
+                times.addLast(stopwatch.elapsedTime());
+                opCounts.addLast(ops);
+                tick += 1;
+            }
+        }
+
+        printTimingTable(Ns, times, opCounts);
     }
 }
